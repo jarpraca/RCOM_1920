a77656aedc2b5c6fe91e0f825d0fa8a37f7b3294
jarpraca <jarpraca@gmail.com>
2019-10-10 20:17:11 +0100

    reverted last commit

--- Trabalho1/noncanonical.c
+++ Trabalho1/noncanonical.c
@@ -16,102 +16,41 @@
 #define A_RCV 2
 #define C_RCV 3
 #define BCC_OK 4
-#define STOPS 5
+#define RCV_DATA 5
+#define BCC2_OK 6
+#define STOPS 7
 
 #define FLAG 0x7E
-#define A 0x03
-#define C_SET 0x03
-#define C_UA 0x07
-#define BCC A^C_SET
-#define BCC2 A^C_UA
+#define A_RSP 0x03
+#define A_CMD 0x01
+#define SET 0x03
+#define UA 0x07
+#define DISC 0x0B
+#define ESC 0x7D
+#define ESC1 0x5E
+#define ESC2 0x0D
+#define RR_R1 0x85
+#define RR_R0 0x05
+#define REJ_R1 0x81
+#define REJ_R2 0x01
+#define C_DATA_S0 0x00
+#define C_DATA_S1 0x40
 
 volatile int STOP=FALSE;
 
-void receive_set(int fd){
-	int state=START;
-	int res;
-	char buf[255];
+int open_port(struct termios *oldtio){
+	int fd;
+	struct termios newtio;
 
-    while (state!=STOPS) {       /* loop for input */
-	 	printf("state:%d\n", state);
-		res = read(fd,buf,1);   /* returns after 5 chars have been input */;
-	  	printf(":%04x:%d\n", *buf, res);
-		int msg=buf[0];
-		printf(":%d:%d\n", msg, res);
-		switch(state){
-			case START:
-				if (msg==FLAG)
-					state=FLAG_RCV;
-				break;
-			case FLAG_RCV:
-				if (msg==A)
-					state=A_RCV;
-				else if (msg!=FLAG)
-					state=START;
-				break;
-			case A_RCV:
-				if (msg==C_SET)
-					state=C_RCV;
-				else
-					state=START;
-				break;
-			case C_RCV:
-				if (msg==BCC)
-					state=BCC_OK;
-				else
-					state=START;
-				break;
-			case BCC_OK:
-				if (msg==FLAG)
-					state=STOPS;
-				else
-					state=START;
-				break;
-			default:
-				state=START;
-				break;
-		}
-	}
-}
-
-void send_ua(int fd){
-	int res;
-	char buf2[5];
-	buf2[0] = FLAG;
-	buf2[1] = A;
-	buf2[2] = C_UA;
-	buf2[3] = BCC2;
-	buf2[4] = FLAG;
-
-    res = write(fd,buf2,5);  
-}
-
-int main(int argc, char** argv)
-{
-    int fd;
-    struct termios oldtio,newtio;
-    char buf[255];
-
-		if ((argc < 2) ||
-				((strcmp("/dev/ttyS0", argv[1]) != 0) &&
-				 (strcmp("/dev/ttyS1", argv[1]) != 0) &&
-				 (strcmp("/dev/ttyS2", argv[1]) != 0)))
-		{
-			printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-      exit(1);
-    }
-
-
-  /*
-    Open serial port device for reading and writing and not as controlling tty
-    because we don't want to get killed if linenoise sends CTRL-C.
-  */
-  
+	/*
+		Open serial port device for reading and writing and not as controlling tty
+		because we don't want to get killed if linenoise sends CTRL-C.
+	*/
     
     fd = open(argv[1], O_RDWR | O_NOCTTY );
     if (fd <0) {perror(argv[1]); exit(-1); }
 
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+    if ( tcgetattr(fd, oldtio) == -1) { /* save current port settings */
       perror("tcgetattr");
       exit(-1);
     }
@@ -127,14 +66,10 @@ int main(int argc, char** argv)
     newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
     newtio.c_cc[VMIN]     = 1;   /* blocking read until 1 chars received */
 
-
-
-  /* 
-    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
-    leitura do(s) pr�ximo(s) caracter(es)
-  */
-
-
+	/* 
+		VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
+		leitura do(s) pr�ximo(s) caracter(es)
+	*/
 
     tcflush(fd, TCIOFLUSH);
 
@@ -144,14 +79,138 @@ int main(int argc, char** argv)
     }
 
     printf("New termios structure set\n");
+}
+
+void  close_port(int fd, struct termios *oldtio){
+	sleep(1);
+    tcsetattr(fd, TCSANOW, oldtio);
+    close(fd);
+}
+
+void receive_msg(int fd, char c, char a, bool data){
+	int state=START;
+	int res;
+	char buf[255];
+	bool escape=false;
+
+    while (state!=STOPS) {       /* loop for input */
+	 	printf("state:%d\n", state);
+		res = read(fd,buf,1);   /* returns after 5 chars have been input */;
+	  	printf(":%04x:%d\n", *buf, res);
+		int msg=buf[0];
+		printf(":%d:%d\n", msg, res);
+		switch(state){
+			case START:
+				if (msg==FLAG)
+					state=FLAG_RCV;
+				break;
+			case FLAG_RCV:
+				if (msg==a)
+					state=A_RCV;
+				else if (msg!=FLAG)
+					state=START;
+				break;
+			case A_RCV:
+				if (msg==c)
+					state=C_RCV;
+				else
+					state=START;
+				break;
+			case C_RCV:
+				if (msg==a^c)
+					state=BCC_OK;
+				else
+					state=START;
+				break;
+			case BCC_OK:
+				if (msg==FLAG){
+					state=STOPS;
+				} 
+				else if (data){
+					state=RCV_DATA;
+					if (msg==ESC)
+						escape=true;
+					else
+						escape=false;//save data
+				}
+				else
+					state=START;
+				break;
+			case RCV_DATA:
+				if (msg==FLAG){
+					state=STOPS;
+					break;
+				} 
+				if (escape)
+				{
+					if (msg==ESC1)
+						escape=false; //save data (0x7E)
+					else if (msg==ESC2)
+						escape=false; //save data (0x7D)
+					else
+						escape=false; //ERRO
+					escape=false;
+					break;
+				}
+				//save data (msg)
+				break;
+			default:
+				state=START;
+				break;
+		}
+	}
+}
+
+void receive_set(int fd){
+	receive_msg(fd, SET, A_RSP);
+}
+
+void receive_disc(int fd){
+	receive_msg(fd, DISC, A_RSP);
+}
+
+void receive_ua(int fd){
+	receive_msg(fd, SET, A_CMD);
+}
+
+void send_resp(int fd, char c, char a){
+	int res;
+	char buf2[5];
+	buf2[0] = FLAG;
+	buf2[1] = a;
+	buf2[2] = c;
+	buf2[3] = a^c;
+	buf2[4] = FLAG;
+
+    res = write(fd,buf2,5);
+}
+
+void send_ua(int fd){
+	send_resp(fd, UA, A_RSP);
+}
+
+void send_disc(int fd){
+	send_resp(fd, DISC, A_CMD);
+}
+
+int main(int argc, char** argv)
+{
+    int fd;
+    struct termios oldtio;
+
+    if ( (argc < 2) || 
+  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
+  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+      exit(1);
+    }
+
+	fd = open_port(&oldtio);
 	
-	sleep(10);
 	receive_set(fd);
-	
 	send_ua(fd);
 
-	sleep(1);
-    tcsetattr(fd,TCSANOW,&oldtio);
-    close(fd);
+	close_port(fd, &oldtio);
+
     return 0;
 }
--- Trabalho1/writenoncanonical.c
+++ Trabalho1/writenoncanonical.c
@@ -6,8 +6,6 @@
 #include <termios.h>
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
-#include <signal.h>
 
 #define BAUDRATE B38400
 #define MODEMDEVICE "/dev/ttyS1"
@@ -15,71 +13,23 @@
 #define FALSE 0
 #define TRUE 1
 #define SIZE 255
-
-#define START 0
-#define FLAG_RCV 1
-#define A_RCV 2
-#define C_RCV 3
-#define UA_RCV 3
-#define BCC_OK 4
-#define STOPS 5
-
 #define FLAG 0x7E
 #define A 0x03
 #define C_SET 0x03
-#define C_UA 0x07
 #define BCC A^C_SET
-#define BCC2 A^C_UA
-
-#define MAX_RETR 3
-#define TIMEOUT 3
 
 volatile int STOP=FALSE;
-int fd;
-int state=START;
-static int num_retr = 0;
-
-void send_set(){
-  char buf[255];
-	buf[0]=FLAG;
-	buf[1] =A;
-	buf[2] = C_SET;
-	buf[3] = BCC;
-	buf[4] = FLAG;
-	
-  write(fd,buf,5);  
-}
-
-void alarmHandler(){
-	if(state>= UA_RCV)
-		return;
-	if(num_retr < MAX_RETR){
-		printf("alarm %d \n", num_retr);
-		send_set();
-		alarm(TIMEOUT);
-		num_retr++;
-	}
-	else
-	{
-		exit(1);
-	}
-	
-}
 
 int main(int argc, char** argv)
 {
-	signal(SIGALRM, alarmHandler);
-	
-    int c, res;
+    int fd,c, res;
     struct termios oldtio,newtio;
     int i, sum = 0, speed = 0;
-
-		if ((argc < 2) ||
-				((strcmp("/dev/ttyS0", argv[1]) != 0) &&
-				 (strcmp("/dev/ttyS1", argv[1]) != 0) &&
-				 (strcmp("/dev/ttyS2", argv[1]) != 0)))
-		{
-			printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+    
+    if ( (argc < 2) || 
+  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
+  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
       exit(1);
     }
 
@@ -122,67 +72,31 @@ int main(int argc, char** argv)
 
     printf("New termios structure set\n");
 
+    char buf[255];
+    buf[0]=FLAG;
+    buf[1] =A;
+    buf[2] = C_SET;
+    buf[3] = BCC;
+    buf[4] = FLAG;
 
-	send_set();
-	alarm(TIMEOUT);
+    res = write(fd,buf,5);  
 
-  /* 
-    O ciclo FOR e as instru��es seguintes devem ser alterados de modo a respeitar 
-    o indicado no gui�o 
-  */
-
-	char buf[255];
-
-    while (state!=STOPS) {       /* loop for input */
-	 	printf("state:%d\n", state);
-		res = read(fd,buf,1);   /* returns after 5 chars have been input */;
-	  	printf(":%04x:%d\n", *buf, res);
-		int msg=buf[0];
-		printf(":%d:%d\n", msg, res);
-		switch(state){
-			case START:
-				if (msg==FLAG)
-					state=FLAG_RCV;
-				break;
-			case FLAG_RCV:
-				if (msg==A)
-					state=A_RCV;
-				else if (msg!=FLAG)
-					state=START;
-				break;
-			case A_RCV:
-				if (msg==C_UA)
-					state=C_RCV;
-				else
-					state=START;
-				break;
-			case C_RCV:
-				if (msg==BCC)
-					state=BCC_OK;
-				else
-					state=START;
-				break;
-			case BCC_OK:
-				if (msg==FLAG)
-					state=STOPS;
-				else
-					state=START;
-				break;
-			default:
-				state=START;
-				break;
-		}
-	}
+	  while (STOP==FALSE) {
+    	res = read(fd,buf,255);
+		  buf[res]=0;		
+		  printf(":%s:%d\n", buf, res);		
+		  if (buf[res-1]=='\0'){
+			  STOP=TRUE;    	
+		  }
+    }
 
 	sleep(1);
    
-    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
-    }
+  if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
 
-    close(fd);
-    return 0;
+  close(fd);
+  return 0;
 }
-
-
