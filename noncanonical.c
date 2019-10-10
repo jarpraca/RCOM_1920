ca1c65de8e6b749443f75a646d765e9f3df2f45a
leonormsousa <20341608+leonormsousa@users.noreply.github.com>
2019-10-10 15:12:25 +0100

    Organised noncanonical.c (open and close functions)

--- noncanonical.c
+++ noncanonical.c
@@ -26,6 +26,55 @@
 
 volatile int STOP=FALSE;
 
+int open_port(struct termios *oldtio){
+	int fd;
+	struct termios newtio;
+
+	/*
+		Open serial port device for reading and writing and not as controlling tty
+		because we don't want to get killed if linenoise sends CTRL-C.
+	*/
+    
+    fd = open(argv[1], O_RDWR | O_NOCTTY );
+    if (fd <0) {perror(argv[1]); exit(-1); }
+
+    if ( tcgetattr(fd, oldtio) == -1) { /* save current port settings */
+      perror("tcgetattr");
+      exit(-1);
+    }
+
+    bzero(&newtio, sizeof(newtio));
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    /* set input mode (non-canonical, no echo,...) */
+    newtio.c_lflag = 0;
+
+    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 1;   /* blocking read until 1 chars received */
+
+	/* 
+		VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
+		leitura do(s) pr�ximo(s) caracter(es)
+	*/
+
+    tcflush(fd, TCIOFLUSH);
+
+    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
+
+    printf("New termios structure set\n");
+}
+
+void  close_port(int fd, struct termios *oldtio){
+	sleep(1);
+    tcsetattr(fd, TCSANOW, oldtio);
+    close(fd);
+}
+
 void receive_msg(int fd, char c){
 	int state=START;
 	int res;
@@ -104,8 +153,7 @@ void send_disc(int fd){
 int main(int argc, char** argv)
 {
     int fd;
-    struct termios oldtio,newtio;
-    char buf[255];
+    struct termios oldtio;
 
     if ( (argc < 2) || 
   	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
@@ -114,51 +162,12 @@ int main(int argc, char** argv)
       exit(1);
     }
 
-  /*
-    Open serial port device for reading and writing and not as controlling tty
-    because we don't want to get killed if linenoise sends CTRL-C.
-  */
-    
-    fd = open(argv[1], O_RDWR | O_NOCTTY );
-    if (fd <0) {perror(argv[1]); exit(-1); }
-
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-      perror("tcgetattr");
-      exit(-1);
-    }
-
-    bzero(&newtio, sizeof(newtio));
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = 0;
-
-    /* set input mode (non-canonical, no echo,...) */
-    newtio.c_lflag = 0;
-
-    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 1;   /* blocking read until 1 chars received */
-
-	/* 
-		VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
-		leitura do(s) pr�ximo(s) caracter(es)
-	*/
-
-    tcflush(fd, TCIOFLUSH);
-
-    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
-    }
-
-    printf("New termios structure set\n");
+	fd = open_port(&oldtio);
 	
-	sleep(10);
 	receive_set(fd);
-	
 	send_ua(fd);
 
-	sleep(1);
-    tcsetattr(fd,TCSANOW,&oldtio);
-    close(fd);
+	close_port(fd, &oldtio);
+
     return 0;
 }
